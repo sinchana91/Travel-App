d4fa24517ab4f32aee65fee76c5a2f4c5da6e19e
sinchana c poojary <sinchanac91@gmail.com>
2024-06-03 22:50:27 +0530

    server setup made

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+
+//routes importing
+// const userRoute = require('./routes/userRoute');
+// const bookingRoute = require('./routes/bookingRoute');
+// const DestinationRoute = require('./routes/DestinationRoute');
+// const reviewRoute = require('./routes/reviewRoute');
+
+// route use
+// app.use('/api/user', userRoute);
+// app.use('/api/booking', bookingRoute);
+// app.use('/api/destination', DestinationRoute);
+// app.use('/api/review', reviewRoute);
+
+ const PORT=process.env.PORT || 3000;
+ app.listen(PORT,()=>{
+     console.log(`Server is running at port ${PORT}`);
+ });
+
+
