775a3031e2928f1e86d5a4d083cd5b5f0358b079
sinchana c poojary <sinchanac91@gmail.com>
2024-06-04 21:02:57 +0530

    UserRoute Made

---
+++ routes/userRoute.js
@@ -0,0 +1,13 @@
+import express from 'express';   
+import { signUp,login,getUser,getUserByName,updateUser,getAllUsers} from '../controllers/userController.js';
+import { auth } from '../middlewares/authMiddleware.js';
+import {router} from 'express.Router';
+
+router.post('/signup', signUp);
+router.post('/login', login);
+router.get('/:id', auth,getUser);
+router.get('/:name', getUserByName);
+router.put('/update/:id', updateUser);
+router.get('/', getAllUsers);
+
+export default router;
